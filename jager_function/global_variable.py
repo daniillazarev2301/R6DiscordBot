41a8a6481e64e952d3c6260e4a6178f58914f97c
daniillazarev2301 <63389915+daniillazarev2301@users.noreply.github.com>
2020-11-15 14:59:29 +0300

    Update global_variable.py

--- jager_function/global_variable.py
+++ jager_function/global_variable.py
@@ -2,6 +2,6 @@ import r6sapi as api
 from jager_function import jager_phrases as phrases
 
 EU = api.RankedRegions.EU
-jager_email = "hunterbot.jager@bk.ru"
-jager_password = "Jagerthebest01"
-channel_memory_id = 701698041660309574
+jager_email = "daniillazarev2301@gmail.com"
+jager_password = "123450000"
+channel_memory_id = 736922459605041183
